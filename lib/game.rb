077d49d487acd03809bb03524e00949cdc1fd0c3
Nick Woods <nicholas.andrew.woods@gmail.com>
2018-08-01 15:45:25 -0700

    Add state variables to be used for castling

--- lib/game.rb
+++ lib/game.rb
@@ -6,6 +6,7 @@ class Game
   def initialize
     @board = Board.new
     @current_player = :white
+    initialize_castle_variables
     
     # if pawn moves 2 spaces, keep track of column for next turn
     @en_passant = nil 
@@ -18,6 +19,7 @@ class Game
   def move(old_sq, new_sq)
     return false if !@board.move(old_sq, new_sq, @current_player, @en_passant)
     @en_passant = (two_rank_pawn_move?(old_sq, new_sq) ? old_sq[0] : nil)
+    update_castle_variables(old_sq, new_sq)
     @current_player = (@current_player == :white ? :black : :white)
     true
   end
@@ -30,4 +32,31 @@ class Game
     @board.piece_at_sq(new_sq).is_a?(Pawn) && old_sq[1] == start_row &&
         new_sq[1] == end_row
   end
+  
+  def initialize_castle_variables
+    @white_k_castle = true
+    @white_q_castle = true
+    @black_k_castle = true
+    @black_q_castle = true
+  end
+  
+  def update_castle_variables(old_sq, new_sq)
+    if @board.piece_at_sq(new_sq).is_a? King
+      if @current_player == :white
+        @white_k_castle = false
+        @white_q_castle = false
+      else
+        @black_k_castle = false
+        @black_q_castle = false
+      end
+    end
+    
+    if @board.piece_at_sq(new_sq).is_a? Rook
+      @white_k_castle = false if old_sq == 'h1'
+      @white_q_castle = false if old_sq == 'a1'
+      @black_k_castle = false if old_sq == 'h8'
+      @black_q_castle = false if old_sq == 'a8'
+    end
+  end
+  
 end
